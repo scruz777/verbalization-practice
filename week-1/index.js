ad64aac36fbbb368eff5129f85a3ea234e44ce4a
scruz777 <sarah.cruz0522@gmail.com>
2024-11-03 19:53:54 -0700

    Update index.js

--- week-1/index.js
+++ week-1/index.js
@@ -3,7 +3,7 @@ var movies = [
         title: 'Rear Window',
         director: 'Alfred Hitchcock',
         year: 1954
-    },
+    },.tit
     {
         title: 'Cape Fear',
         director: 'Martin Scorsese',
@@ -22,7 +22,10 @@ var movies = [
 ]
 
 function createWeirdTitle(array){
-  
-}
+var x1 = array[0].title.split(" ")[1];//"Window" from "Rear Window";
+var x2 = array[1].title.split(" ")[0];//"Cape" from "Cape Fear";
+var x3 = array[2].title.split(" ")[0];//"Fright" from "Fright Night";
+var x4 = array[3].title.split(" ")[1];//"Velvet" from "Blue Velvet";
+    return x1 + x2 + x3 + x4
 
-
+console.log(createWeirdTitle);
